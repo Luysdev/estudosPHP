dd6354b8daa3f8406065af7e50a4189527d93c00
Luís Eduardo da Silva Batista <119604941+Luysdev@users.noreply.github.com>
2023-12-26 13:03:13 -0300

    inicio do front

--- loja1/model/UserModel.php
+++ loja1/model/UserModel.php
@@ -1,1 +1,82 @@
 <?php
+class UserModel{
+    private $name_user;
+    private $password_user;
+    private $perfil_user;
+
+    public function __construct($name_user,$password_user)
+    {
+           $this->name_user = $name_user;
+           $this->password_user = $password_user;
+           $this->perfil_user = 2;
+           // 2 para usuarios normais e 1 para admins
+    }
+
+    public function insertUser($db)
+    {
+        $query = "INSERT INTO user (name_user,password_user,perfil_user) VALUES (?, ?, ?)";
+
+        $stmt = mysqli_prepare($db, $query);
+
+        // Vincular parâmetros
+        mysqli_stmt_bind_param($stmt, "ssi", $this->name_user, $this->password_user, $this->perfil_user);
+
+        // Executar a declaração
+        $result = mysqli_stmt_execute($stmt);
+
+        // Verificar o resultado
+        if ($result) {
+            return "User: " . $this->name_user . " adicionado com sucesso";
+        } else {
+            return "Erro ao adicionar user: " . mysqli_error($db);
+        }
+        mysqli_stmt_close($stmt);
+    }
+
+    public function deleteCliente($db, $name_user){
+        $query = "DELETE FROM user WHERE name_user = ?";
+
+        $stmt = mysqli_prepare($db, $query);
+
+        // Vincular parâmetros
+        mysqli_stmt_bind_param($stmt, "s", $name_user);
+
+        // Executar a declaração
+        $result = mysqli_stmt_execute($stmt);
+
+        // Verificar o resultado
+        if ($result) {
+            return "User deletado com sucesso";
+        } else {
+            return "Erro ao deletar user: " . mysqli_error($db);
+        }
+
+        // Fechar a declaração
+        mysqli_stmt_close($stmt);
+    }
+
+    public function selectCliente($db, $name_user){
+        $query = "SELECT * FROM user WHERE name_user = ?";
+
+        $stmt = mysqli_prepare($db, $query);
+
+        // Vincular parâmetros
+        mysqli_stmt_bind_param($stmt, "s", $name_user);
+
+        // Executar a declaração
+        mysqli_stmt_execute($stmt);
+
+        // Obter resultado
+        $result = mysqli_stmt_get_result($stmt);
+
+        // Verificar se há resultados
+        if ($row = mysqli_fetch_assoc($result)) {
+            return $row;
+        } else {
+            return null;
+        }
+
+        // Fechar a declaração
+        mysqli_stmt_close($stmt);
+    }
+}
--- loja1/model/teste.php
+++ loja1/model/teste.php
@@ -2,9 +2,11 @@
 global $conn;
 include "ClienteModel.php";
 include "db.php";
+include "UserModel.php";
 $new_cliente = new ClienteModel("jerson", "luis@gmail.com","1234123412","2000-01-01");
+$new_user = new UserModel("Luis Eduardo", 12341234);
 
-
+$new_user->deleteCliente($conn,"Luis Eduardo");
 $result = $new_cliente -> selectCliente($conn, '12351351');
 
 print_r($result);
